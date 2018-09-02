a5c41c5a8987755435741744c782fd7423f1a492
tokunagawataru <tokunaga.wataru31@gmail.com>
2018-09-03 03:10:06 +0900

    add file close

--- snippets/ioReader/readFromFile/main.go
+++ snippets/ioReader/readFromFile/main.go
@@ -8,9 +8,28 @@ import (
 
 func main() {
 	file, err := os.Open("test.txt")
-	buffer, err := ioutil.ReadAll(file)
 	if err != nil {
 		fmt.Fprintf(os.Stdout, "open file err: %v\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	buf := make([]byte, 1024)
+	if _, err := file.Read(buf); err != nil {
+		fmt.Fprintf(os.Stdout, "read file err: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(string(buf))
+
+	file2, err := os.Open("test2.txt")
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "open file err: %v\n", err)
+		os.Exit(1)
+	}
+	defer file2.Close()
+	buffer, err := ioutil.ReadAll(file2)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "read file err: %v\n", err)
 		os.Exit(1)
 	}
 	fmt.Println(string(buffer))
