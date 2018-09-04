3e3e21c41ed78e8d14ca18aced13f745e9d6bc5d
tokunagawataru <tokunaga.wataru31@gmail.com>
2018-09-05 02:40:53 +0900

    add read multi string

---
+++ snippets/readFromMultiString/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"os"
+)
+
+func main() {
+	l1 := bytes.NewBufferString("line1\n")
+	l2 := bytes.NewBufferString("line2\n")
+	l3 := bytes.NewBufferString("line3\n")
+	r := io.MultiReader(l1, l2, l3)
+	io.Copy(os.Stdout, r)
+}
