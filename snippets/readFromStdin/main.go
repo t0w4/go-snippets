7c97c11815f97b4f9fd7789ccb856b808fc8eeb7
tokunagawataru <tokunaga.wataru31@gmail.com>
2018-09-05 00:42:28 +0900

    add read from stdin

---
+++ snippets/readFromStdin/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		line, err := reader.ReadString('\n')
+		fmt.Printf("%#v\n", line)
+		if err == io.EOF {
+			break
+		}
+	}
+
+	// ex2
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintf(os.Stdout, "read file err : %v", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%#v\n", scanner.Text())
+	}
+
+	// ex3
+	rd := bufio.NewReader(os.Stdin)
+	for {
+		line, _, err := rd.ReadLine()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "read line err : %v", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%#v\n", string(line))
+	}
+}
