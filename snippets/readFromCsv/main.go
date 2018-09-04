636467962fc599ddb98f55b77239708b7a75ac39
tokunagawataru <tokunaga.wataru31@gmail.com>
2018-09-05 02:17:13 +0900

    add read from csv

---
+++ snippets/readFromCsv/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("test.csv")
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "open file err : %v", err)
+		os.Exit(1)
+	}
+	r := bufio.NewReader(f)
+	cr := csv.NewReader(r)
+	for {
+		l, err := cr.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "read csv err : %v", err)
+			os.Exit(1)
+		}
+		fmt.Println(l[2], l[6:9])
+	}
+}
