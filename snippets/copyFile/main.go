95c61e818480d1c1cbced7a2826d64fd5759c5b0
tokunagawataru <tokunaga.wataru31@gmail.com>
2018-09-06 01:38:20 +0900

    add copy file

---
+++ snippets/copyFile/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	fo, err := os.Open("old.txt")
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "open file err : %v", err)
+		os.Exit(1)
+	}
+	defer fo.Close()
+	fn, err := os.Create("new.txt")
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "open file err : %v", err)
+		os.Exit(1)
+	}
+	defer fn.Close()
+	s, err := io.Copy(fn, fo)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "copy file err : %v, size: %d", err, s)
+		os.Exit(1)
+	}
+}
