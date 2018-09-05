246854bcf5b014d5eca9dcaf40432c858c470cc3
tokunagawataru <tokunaga.wataru31@gmail.com>
2018-09-06 01:39:52 +0900

    add create random byte file

---
+++ snippets/createRandomFile/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	"os"
+)
+
+func main() {
+	c := 1024
+	b := make([]byte, c)
+	_, err := rand.Read(b)
+	if err != nil {
+		fmt.Printf("read rand bytes err: %v", err)
+		os.Exit(1)
+	}
+
+	f, err := os.Create("tmp.txt")
+	if err != nil {
+		fmt.Printf("cread file err: %v", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+	n, err := f.Write(b)
+	if err != nil {
+		fmt.Printf("write file err: %v, byte: %d", err, n)
+		os.Exit(1)
+	}
+}
