34af5171d334d44cec6e6c65bdba9072f9024d41
tokunagawataru <tokunaga.wataru31@gmail.com>
2018-09-08 02:48:56 +0900

    add simple catch signal

---
+++ snippets/simpleCatchSignal/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+// if you want to check SIGTERM, you must build.
+func main() {
+	signals := make(chan os.Signal, 1)
+	// catch SIGINT(Ctrl+C), KILL signal
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	fmt.Println("waiting SIGNAL")
+	<-signals
+	fmt.Println("catch SIGNAL")
+}
