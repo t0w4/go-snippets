a453b2e60b0310e86bf114b15610e92e5e38ca79
tokunagawataru <tokunaga.wataru31@gmail.com>
2018-09-09 15:11:47 +0900

    add simple tcp server

---
+++ snippets/simpleTcpServer/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "Listen handle error: %v\n", err)
+		os.Exit(1)
+	}
+	defer li.Close()
+
+	// repeat Accept
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "Accept handle error : %v\n", err)
+			break
+		}
+		go func() {
+			buffer, err := ioutil.ReadAll(conn)
+			if err != nil {
+				fmt.Fprintf(os.Stdout, "read request err: %v\n", err)
+				os.Exit(1)
+				conn.Close()
+			}
+			fmt.Println(string(buffer))
+			conn.Close()
+		}()
+	}
+}
