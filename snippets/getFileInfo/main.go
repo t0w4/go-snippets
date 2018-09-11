613eeed9f6128b68e0d0e3891ee0cd40671f7685
tokunagawataru <tokunaga.wataru31@gmail.com>
2018-09-11 23:58:43 +0900

    get file info

---
+++ snippets/getFileInfo/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fileName := "test.txt"
+	file, err := os.Stat(fileName)
+	if err == os.ErrNotExist {
+		fmt.Printf("%s is not exist\n", fileName)
+		os.Exit(1)
+	} else if err != nil {
+		fmt.Printf("file stat error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(file)
+}
