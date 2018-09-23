0116c2ebd99d5e9ef7e865a672ba5a141cad8d4c
tokunagawataru <tokunaga.wataru31@gmail.com>
2018-09-23 14:49:01 +0900

    add exec command

---
+++ snippets/execCommand/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("At least one argument is required.")
+		os.Exit(1)
+	}
+	cmd := exec.Command(os.Args[1], os.Args[2:]...)
+
+	// cmd.Run(): cmd.Start() + cmd.Wait()
+	//err := cmd.Run()
+	// cmd.Output(): Run() + Stdout
+	// cmd.CombinedOutput(): Run() + Stdout + Stderr
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("exec command error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("--------Output Start--------")
+	fmt.Print(string(output))
+	fmt.Println("--------Output END  --------")
+
+	state := cmd.ProcessState
+	fmt.Printf("%s\n", state.String())
+	fmt.Printf(" Pid: %d\n", state.Pid())
+	fmt.Printf(" System: %v\n", state.SystemTime())
+	fmt.Printf(" User: %v\n", state.UserTime())
+}
